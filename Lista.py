3c9424112b1200f7ed682a552554fc61d0e7134f
GustavoLeao2018 <gustavo.leao.nogueira.2018@gmail.com>
2018-12-13 01:41:49 -0200

    Tudo terminado!

--- Lista.py
+++ Lista.py
@@ -4,6 +4,7 @@
 from No import No
 
 
+
 class Lista:
     def __init__(self):
         self.inicial = None
@@ -54,33 +55,50 @@ class Lista:
         self._organize()
 
     def _organize(self):
-        iterador = self.inicial
-        anterior = None
-        while iterador is not None:
-            if iterador is not None:
-                if anterior is not None:
-                    print()
-                    print("Iterador: "+str(iterador.dado))
-                    print("Anterior: "+str(anterior.dado))
-                    print()    
-                    if iterador.dado < anterior.dado:
-                        print()
-                        print("Iterador: "+str(iterador.dado))
-                        print("Anterior: "+str(anterior.dado))
-                        print()    
-            anterior = iterador            
-            iterador = iterador.proximo
+        pass
+        # print("*"*20)
+        # iterador = self.inicial
+        # anterior = None
+        # while iterador is not None:
+        #     if iterador.anterior is not None:
+        #             if iterador.anterior.dado < iterador.dado:
+        #                 print(f"{iterador.anterior.dado} is not  None =  {iterador.anterior is not None}")
+        #                 # print(f"\u001b[33m {str(iterador.anterior.dado)}  < {str(iterador.dado)} \u001b[0m")
+        #             elif iterador.anterior.dado > iterador.dado:
+        #                 print(f"{iterador.anterior.dado} is not  None =  {iterador.anterior is not None}")
+        #                 if iterador.anterior is not None and iterador.proximo is not None:
+        #                     anterior = iterador.anterior
+        #                     no = iterador
+        #                     proximo = iterador.proximo
+        #                     print(f"\u001b[33m {anterior.dado} > {no.dado} and {anterior.dado} > {proximo.dado}  \u001b[0m")
+        #                     print(f"\u001b[33m {anterior.dado > no.dado and anterior.dado > proximo.dado}  \u001b[0m")
+        #
+        #                     if anterior.dado > no.dado and anterior.dado > proximo.dado:
+        #                         iterador = no
+        #                         iterador.anterior =  proximo
+        #                         iterador.proximo = anterior
+        #
+        #
+        #                     print(anterior)
+        #                     print(no)
+        #                     print(proximo)
+        #
+        #
+        #                     #print(f"\u001b[34m {str(iterador.anterior.dado)}  < {str(iterador.dado)} \u001b[0m")
+        #     iterador = iterador.proximo
+        # print("*"*20)
 
     def search(self, valor):
-        print("#" * 100)
         if self.saltar is False:
             salto_1 = self.quantidade_de_itens // 2
             salto_2 = salto_1 // 2
             salto_3 = salto_2 // 2
+            print("*" * 20)
             print(f"Quantidade de itens: {self.quantidade_de_itens}")
             print(f"Salto 1: {salto_1}")
             print(f"Salto 2: {salto_2}")
             print(f"Salto 3: {salto_3}")
+            print("*" * 20)
 
             iterador = self.inicial
             cont = 0
@@ -123,10 +141,10 @@ class Lista:
                 else:
                     anterior.salto_2 = item
 
-
                 anterior = item
             self.saltar = True
 
+        print("Salto 1:")
         iterador = self.inicial.salto_1
         while iterador is not None:
             if iterador.proximo is not None:
@@ -155,24 +173,69 @@ class Lista:
                     break
 
             iterador = iterador.salto_1
-        print("#" * 100)
+
+            print("Salto 2:")
+            iterador = self.final.salto_2
+            while iterador is not None:
+                if iterador.proximo is not None:
+                    if valor == iterador.proximo.dado:
+                        print(f"{valor} <  {str(iterador.proximo.dado)} = {valor == iterador.proximo.dado}")
+                if iterador.anterior is not None:
+                    if valor == iterador.anterior.dado:
+                        print(f"{valor} <  {str(iterador.anterior.dado)} = {valor == iterador.anterior.dado}")
+                if valor == iterador.dado:
+                    print(f"{str(iterador.dado)} ==  {valor} = {valor == iterador.dado}")
+                    break
+                if valor < iterador.dado:
+                    print(f"{valor} <  {str(iterador.dado)} = {valor < iterador.dado}")
+                    if valor == iterador.anterior.dado:
+                        print(f"{str(iterador.anterior.dado)} ==  {valor} = {valor == iterador.anterior.dado}")
+                        break
+                    if valor is not iterador.anterior.dado:
+
+                        iterador_volta = iterador.anterior
+                        while iterador_volta.anterior:
+                            if valor == iterador_volta.dado:
+                                print(f"{str(iterador_volta.dado)} ==  {valor} = {valor == iterador_volta.dado}")
+                                break
+
+                            iterador_volta = iterador_volta.anterior
+                        break
+
+                iterador = iterador.salto_2
+
+
+            print("Salto 3:")
+            iterador = self.inicial.salto_3
+            while iterador is not None:
+                if iterador.proximo is not None:
+                    if valor == iterador.proximo.dado:
+                        print(f"{valor} <  {str(iterador.proximo.dado)} = {valor == iterador.proximo.dado}")
+                if iterador.anterior is not None:
+                    if valor == iterador.anterior.dado:
+                        print(f"{valor} <  {str(iterador.anterior.dado)} = {valor == iterador.anterior.dado}")
+                if valor == iterador.dado:
+                    print(f"{str(iterador.dado)} ==  {valor} = {valor == iterador.dado}")
+                    break
+                if valor < iterador.dado:
+                    print(f"{valor} <  {str(iterador.dado)} = {valor < iterador.dado}")
+                    if valor == iterador.anterior.dado:
+                        print(f"{str(iterador.anterior.dado)} ==  {valor} = {valor == iterador.anterior.dado}")
+                        break
+                    if valor is not iterador.anterior.dado:
+
+                        iterador_volta = iterador.anterior
+                        while iterador_volta.anterior:
+                            if valor == iterador_volta.dado:
+                                print(f"{str(iterador_volta.dado)} ==  {valor} = {valor == iterador_volta.dado}")
+                                break
+
+                            iterador_volta = iterador_volta.anterior
+                        break
+
+                iterador = iterador.salto_3
 
     def __str__(self):
-        # iterador = self.inicial
-        # texto = "#" * 20 + "[Lista:]" + "#" * 20 + "\n"
-        # while iterador is not None:
-        #     texto += f"{str(iterador)}\n"
-        #     if iterador.salto_1 is not None:
-        #         texto += f"\u001b[32m-> Salto 1: {iterador.salto_1} \u001b[0m\n"
-        #     if iterador.salto_2 is not None:
-        #         texto += f"\u001b[33m--> Salto 2: {iterador.salto_2} \u001b[0m\n"
-        #     if iterador.salto_3 is not None:
-        #         texto += f"\u001b[34m---> Salto 3: {iterador.salto_3} \u001b[0m\n"
-        #     iterador = iterador.proximo
-        # texto += f"Tamanho: {self.quantidade_de_itens} \n"
-        # texto += "#" * 20 + "#" * 5 + "#" * 20
-        # return texto
-
         iterador = self.inicial
         texto = "#" * 20 + "[Lista:]" + "#" * 20 + "\n"
         while iterador is not None:
--- Main.py
+++ Main.py
@@ -1,24 +1,21 @@
 from Lista import Lista
 from random import randint
 
-lista = Lista()
+# lista = Lista()
+#
+# for contador in range(101):
+#     lista.append(contador)
 
-for contador in range(11):
-    lista.append(randint(0, 100))
+# print(lista)
 
-print(lista)
-
-
-# print("\n\n\n\n\n")
-# lista.search(randint(0, 100))
-# lista.search(randint(0, 100))
 
 # print(lista.pop())
+#
+#
+# for i in range(randint(10, 100)):
+#     print("\n")
+#     lista.search(randint(0, 100))
+#     print("\n")
+#
 
-# lista.search(randint(0, 100))
-# lista.search(randint(0, 100))
-# lista.search(100)
-
-
-# print("\n\n\n\n\n")
 # print(lista)
